9cc4859c2a98a9a88f58f37705ac725135347010
Alessio <alessio.livolsi.27@gmail.com>
2021-09-06 12:14:00 +0100

    End of part 1

--- snippets/views.py
+++ snippets/views.py
@@ -6,12 +6,31 @@ from snippets.serializers import SnippetSerializer
 
 
 @csrf_exempt
-def snippet_list(request, pk):
+def snippet_list(request):
+    """
+    List all code snippets, or create a new snippet.
+    """
+    if request.method == "GET":
+        snippets = Snippet.objects.all()
+        serializer = SnippetSerializer(snippets, many=True)
+        return JsonResponse(serializer.data, safe=False)
+
+    elif request.method == "POST":
+        data = JSONParser().parse(request)
+        serializer = SnippetSerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data, status=201)
+        return JsonResponse(serializer.errors, status=400)
+
+
+@csrf_exempt
+def snippet_detail(request, pk):
     """
     Retrieve, update or delete a code snippet.
     """
     try:
-        snippet = Snippet.object.get(pk=pk)
+        snippet = Snippet.objects.get(pk=pk)
     except Snippet.DoesNotExist:
         return HttpResponse(status=404)
 
