283642aadfa5249ff777eba2045966b5abe25d42
Alessio <alessio.livolsi.27@gmail.com>
2021-09-07 09:53:56 +0100

    Created an endpoint for the highlighted snippets

--- snippets/views.py
+++ snippets/views.py
@@ -1,10 +1,11 @@
-from snippets.models import Snippet
-from snippets.serializers import SnippetSerializer
-from rest_framework import generics
 from django.contrib.auth.models import User
-from snippets.serializers import UserSerializer
-from rest_framework import permissions
+from rest_framework import generics, permissions, renderers
+from rest_framework.decorators import api_view
+from rest_framework.response import Response, reverse
+
+from snippets.models import Snippet
 from snippets.permissions import IsOwnerOrReadOnly
+from snippets.serializers import SnippetSerializer, UserSerializer
 
 
 class SnippetList(generics.ListCreateAPIView):
@@ -32,3 +33,22 @@ class UserList(generics.ListAPIView):
 class UserDetail(generics.RetrieveAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+
+
+@api_view
+def api_root(request, format=None):
+    return Response(
+        {
+            "users": reverse("user-list, request=request, format=format"),
+            "snippets": reverse("snippet-list", request=request, format=format),
+        }
+    )
+
+
+class SnippetHighlight(generics.GenericAPIView):
+    queryset = Snippet.objects.all()
+    renderer_classes = [renderers.StaticHTMLRenderer]
+
+    def get(self, request, *args, **kwargs):
+        snippet = self.get_object()
+        return Response(snippet.highlighted)
